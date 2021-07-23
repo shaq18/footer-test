387b9e4e52793ada376551a951254f5c333a127c
Shaquille Galanto <shaquille.galanto18@gmail.com>
2021-07-23 21:47:45 +0800

    feat: update variant styles of link component

--- src/components/navigation/link/Link.tsx
+++ src/components/navigation/link/Link.tsx
@@ -1,14 +1,22 @@
+import clsx from 'clsx';
 import React, { forwardRef, ReactNode } from 'react';
 
 export interface LinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
   children?: ReactNode;
   className?: string;
-  variant?: 'footer' | 'header';
+  variant?: 'footer' | 'footer-sub';
 }
 
+const styles = {
+  variant: {
+    footer: 'font-black text-footer lowercase t-sm:font-normal t-sm:normal-case',
+    'footer-sub': 'text-footer lowercase',
+  },
+};
+
 const Link = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ className, href, variant, ...props }, ref) => {
-    return <a href={href} {...props} ref={ref} />;
+  ({ className, href, variant = 'footer', ...props }, ref) => {
+    return <a href={href} {...props} ref={ref} className={clsx(styles.variant[variant])} />;
   }
 );
 
