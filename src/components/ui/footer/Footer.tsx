f1f43bee73ae96b72bb9d85ca6746f26c852bb84
Shaquille Galanto <shaquille.galanto18@gmail.com>
2021-07-23 19:04:50 +0800

    feat: add forwardRef to footer

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
+import Container from '../../layout/container';
+import Section from '../../layout/section';
 
 interface FooterProps {
   desktopFooter: {};
   mobileFooter: {};
 }
 
-const Footer = ({ desktopFooter, mobileFooter, ...props }: FooterProps) => {
-  console.log(mobileFooter);
-  console.log(desktopFooter);
-  return <footer {...props}></footer>;
-};
+const Footer = forwardRef<HTMLElement, FooterProps>(
+  ({ desktopFooter, mobileFooter, ...props }, ref) => {
+    console.log(mobileFooter);
+    console.log(desktopFooter);
+    return (
+      <footer ref={ref} {...props}>
+        <Section className="bg-primary text-white">
+          <Container>
+            <p>asd</p>
+          </Container>
+        </Section>
+      </footer>
+    );
+  }
+);
 
 export default Footer;
