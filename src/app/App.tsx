7a0b8693513626501b4c61c6746268e9a549dc97
Shaquille Galanto <shaquille.galanto18@gmail.com>
2021-07-23 18:01:04 +0800

    feat: setup tailwindCSS

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Footer from '../components/ui/footer';
-import '../styles/styles.css';
 import { desktopFooter, mobileFooter } from './mockData';
 
 export default () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './styles/index.css';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
